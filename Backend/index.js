70a7d1be401f1bc01fef1f973af4c0e4d1a6e0ac
KeshavPandya01 <keshavpandya01@gmail.com>
2024-06-16 02:43:37 +0530

    started working on admin panel

--- Backend/index.js
+++ Backend/index.js
@@ -110,6 +110,13 @@ app.post("/removeproduct",async (req,res)=>{
         name: req.body.name
     })
 });
+
+//Creating API for getting all products
+app.get("/allproducts", async (req,res)=>{
+    let products = await Product.find({});
+    console.log("All products fetched");
+    res.send(products);
+});
 app.listen(port, (error => {
     if (!error) {
         console.log('Server Running on port ' + port);
